08cd5447cbf6e60191283ba3445bd40ba13820e5
Nazar Ismailov <road2thedream1@gmail.com>
2026-08-31 11:00:16 +0200

    initial

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9bdckBZcy0e4zL542r5QviF4SIWcWGfU",
-  authDomain: "planer-142df.firebaseapp.com",
-  projectId: "planer-142df",
-  storageBucket: "planer-142df.firebasestorage.app",
-  messagingSenderId: "283633401973",
-  appId: "1:283633401973:web:f2dbf4d6bbf41d0cac783c",
-  measurementId: "G-ZJBKFFHN73"
+ apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
